8b3f877b15214c28165af8f0375bd1f1ffbfb67b
gokulkrishnanm804 <gokulkrishnanm830@gmail.com>
2026-03-15 17:39:16 +0530

    FindLargestNumber.java

---
+++ Arrays/FindLargestNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0;i < n;i++) {
+            arr[i] = sc.nextInt();
+        }
+        int high = arr[0];
+        for(int i = 1;i < n;i++) {
+            if(high < arr[i]) {
+                high = arr[i];
+            }
+        }
+        System.out.print(high);
+    }
+}
+
+5
+10 20 30 50 40
+50
