d3c5f19ea029c3b1d5e4db53d330cfbf585284d8
gokulkrishnanm804 <gokulkrishnanm830@gmail.com>
2026-03-15 18:32:22 +0530

    FindingDuplicate.java

---
+++ Arrays/FindingDuplicate.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0;i < n;i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0;i < n;i++) {
+            for(int j = i + 1;j < n;j++) {
+                if(arr[i] == arr[j]) {
+                    System.out.print(arr[i]+" ");
+                }
+            }
+        }
+    }
+}
+
+6
+1 1 2 2 3 4
+1 2 
