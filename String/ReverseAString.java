282c6cbe57004dc11801796d4e719960b8e27d93
gokulkrishnanm804 <gokulkrishnanm830@gmail.com>
2026-03-18 21:07:56 +0530

    ReverseAString.java

---
+++ String/ReverseAString.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String str = sc.nextLine();
+
+        StringBuilder sb = new StringBuilder(str);
+
+        sb.reverse();
+
+        System.out.println("Reversed: " + sb);
+    }
+}
